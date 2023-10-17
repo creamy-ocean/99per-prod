83b0eaed4bf0f99481f0e8aa48e3dc54f1ee40be
Hyeyoung Yun <hyeyoungyun1214@gmail.com>
2023-10-17 21:30:36 +0900

    [Fix] import문 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { App } from "@/App";
+import App from "@/App";
 import { ChakraProvider } from "@chakra-ui/react";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";

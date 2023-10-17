f64c0f75a0842ec96a4efc0811b27f28083ccb91
Hyeyoung Yun <hyeyoungyun1214@gmail.com>
2023-10-17 21:24:25 +0900

    [Chore] 기본 라우터 설정
    - BrowserRouter 설정
    - Root, ErrorPage, Home 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
-export const App = () => {
-  return <></>;
+import router from "@/routes/router";
+import { RouterProvider } from "react-router-dom";
+
+const App = () => {
+  return <RouterProvider router={router} />;
 };
+
+export default App;
---
+++ src/routes/router.tsx
@@ -0,0 +1,20 @@
+import ErrorPage from "@/pages/ErrorPage";
+import Home from "@/pages/Home";
+import Root from "@/pages/Root";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default router;

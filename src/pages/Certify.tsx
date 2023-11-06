390cb2af5a249efc94007dfee675e7fce036490d
Hyeyoung Yun <hyeyoungyun1214@gmail.com>
2023-11-06 18:37:09 +0900

    [Feat] 여성 인증 페이지 구현
    - 여성 인증 페이지 구현
    - 여성 인증 링크 추가

--- src/pages/Certify.tsx
+++ src/pages/Certify.tsx
@@ -1,4 +1,35 @@
+import { Divider, Flex, Heading, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
 const Certify = () => {
-  return <div></div>;
+  return (
+    <Flex
+      justify="center"
+      alignItems="center"
+      h="100vh"
+      bgGradient="linear(brand.100 0%, brand.500 100%)"
+    >
+      <Flex
+        direction="column"
+        alignItems="center"
+        backgroundColor="#E6F2FD"
+        borderRadius="3xl"
+        boxShadow="2xl"
+        px="20"
+        py="4"
+      >
+        <Heading mt="12">여성 인증</Heading>
+        <Text mt="4">99%는 여성만 이용 가능한 서비스로,</Text>
+        <Text>여성 인증 후 이용 가능합니다</Text>
+        <Text mb="8">(인증 완료까지 최대 24시간 소요)</Text>
+        <Divider />
+        <Text p="8" color="brand.500" decoration="underline">
+          <Link to="https://forms.gle/dwNojWQJbd312Lqp8">
+            ✨ 여성 인증 링크 ✨
+          </Link>
+        </Text>
+      </Flex>
+    </Flex>
+  );
 };
 export default Certify;

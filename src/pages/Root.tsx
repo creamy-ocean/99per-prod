bf0647cf083aa468128a2d6830440bde0fc4e873
Hyeyoung Yun <hyeyoungyun1214@gmail.com>
2023-11-07 17:21:43 +0900

    [Feat] Header 컴포넌트 구현
    - Header 컴포넌트 내 Navbar 구현
    - Header 컴포넌트가 로그인 된 경우에만 노출되도록 구현

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,17 +1,29 @@
+import { useAuthContext } from "@/context/AuthContext";
 import { Flex } from "@chakra-ui/react";
+import Header from "@components/common/Header";
 import { Outlet } from "react-router-dom";
 
 const Root = () => {
+  const user = useAuthContext();
   return (
     <>
-      <Flex
-        justify="center"
-        alignItems="center"
-        h="100vh"
-        bgGradient="linear(brand.100 0%, brand.500 100%)"
-      >
-        <Outlet />
-      </Flex>
+      {user?.userState === "certified" ? (
+        <>
+          <Header />
+          <Outlet />
+        </>
+      ) : (
+        <>
+          <Flex
+            justify="center"
+            alignItems="center"
+            h="100vh"
+            bgGradient="linear(brand.100 0%, brand.500 100%)"
+          >
+            <Outlet />
+          </Flex>
+        </>
+      )}
     </>
   );
 };

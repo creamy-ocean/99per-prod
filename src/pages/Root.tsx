097d0e363c35925bfaf84cb214f262bc3f174294
Hyeyoung Yun <hyeyoungyun1214@gmail.com>
2023-11-07 17:06:37 +0900

    [Style] 중복되는 코드 Root 페이지로 이동
    - 중복되는 Flex 코드를 Root 페이지로 이동하여 전체 페이지에 적용될 수 있도록 함

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,10 +1,18 @@
+import { Flex } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 
 const Root = () => {
   return (
-    <div>
-      <Outlet />
-    </div>
+    <>
+      <Flex
+        justify="center"
+        alignItems="center"
+        h="100vh"
+        bgGradient="linear(brand.100 0%, brand.500 100%)"
+      >
+        <Outlet />
+      </Flex>
+    </>
   );
 };
 export default Root;

539ec1aecd0bb41dae3bca9ca99bbe6a11fccbc3
Hyeyoung Yun <hyeyoungyun1214@gmail.com>
2023-11-09 17:50:51 +0900

    [Feat] newProfile 페이지에 탭 구현
    - 친구, 파티, 길드 탭을 클릭하면 그에 맞는 콘텐츠가 노출되도록 구현

--- src/pages/NewProfile.tsx
+++ src/pages/NewProfile.tsx
@@ -10,11 +10,22 @@ import {
   HStack,
   Heading,
   Select,
+  Tab,
+  TabList,
+  Tabs,
   Text,
   Textarea,
 } from "@chakra-ui/react";
+import { useState } from "react";
 
 const NewProfile = () => {
+  const [currTab, setCurrTab] = useState<string>("친구");
+
+  const changeTab = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const eventTarget = e.target as HTMLButtonElement;
+    setCurrTab(eventTarget.innerText);
+  };
+
   return (
     <Flex
       direction="column"
@@ -25,10 +36,17 @@ const NewProfile = () => {
       borderRadius="xl"
       alignItems="center"
     >
-      <Heading fontSize="3xl" color="#555" pt="10" pb="14">
+      <Heading fontSize="3xl" color="#555" pt="6" pb="10">
         새로운 프로필 만들기
       </Heading>
       <Box>
+        <Tabs variant="soft-rounded" colorScheme="blue" mb="4" align="center">
+          <TabList>
+            <Tab onClick={changeTab}>친구</Tab>
+            <Tab onClick={changeTab}>파티</Tab>
+            <Tab onClick={changeTab}>길드</Tab>
+          </TabList>
+        </Tabs>
         <FormControl
           sx={{
             select: { fontSize: "0.9rem" },
@@ -81,7 +99,14 @@ const NewProfile = () => {
               </Checkbox>
             </HStack>
           </CheckboxGroup>
-          <FormHelperText>나의 관심사를 선택해주세요</FormHelperText>
+          <FormHelperText>
+            {currTab === "친구"
+              ? "나"
+              : currTab === "파티"
+              ? "우리 파티"
+              : "우리 길드"}
+            의 관심사를 선택해주세요
+          </FormHelperText>
         </FormControl>
         <FormControl
           sx={{
@@ -90,40 +115,70 @@ const NewProfile = () => {
             },
           }}
         >
-          <FormLabel mt="8" fontWeight="bold" color="#555">
-            플레이 스타일
-          </FormLabel>
-          <CheckboxGroup>
-            <HStack spacing="24px">
-              <Checkbox value="attacker">
-                <Text>돌격형</Text>
-              </Checkbox>
-              <Checkbox value="defender">
-                <Text>방어형</Text>
-              </Checkbox>
-              <Checkbox value="serious">
-                <Text>빡겜</Text>
-              </Checkbox>
-              <Checkbox value="casual">
-                <Text>즐겜</Text>
-              </Checkbox>
-            </HStack>
-            <HStack spacing="24px" mt="2">
-              <Checkbox value="voice">
-                <Text>보이스</Text>
-              </Checkbox>
-              <Checkbox value="chat">
-                <Text>채팅만</Text>
-              </Checkbox>
-              <Checkbox value="interact">
-                <Text>소통</Text>
-              </Checkbox>
-              <Checkbox value="solo">
-                <Text>솔플</Text>
-              </Checkbox>
-            </HStack>
-          </CheckboxGroup>
-          <FormHelperText>나의 플레이 스타일을 선택해주세요</FormHelperText>
+          {currTab === "길드" ? (
+            <>
+              <FormLabel mt="8" fontWeight="bold" color="#555">
+                길드 스타일
+              </FormLabel>
+              <CheckboxGroup>
+                <HStack spacing="24px">
+                  <Checkbox value="friendship">
+                    <Text>친목</Text>
+                  </Checkbox>
+                  <Checkbox value="solo">
+                    <Text>솔플</Text>
+                  </Checkbox>
+                  <Checkbox value="voice">
+                    <Text>보이스</Text>
+                  </Checkbox>
+                  <Checkbox value="chat">
+                    <Text>채팅만</Text>
+                  </Checkbox>
+                </HStack>
+              </CheckboxGroup>
+              <FormHelperText>우리 길드의 스타일을 선택해주세요</FormHelperText>
+            </>
+          ) : (
+            <>
+              <FormLabel mt="8" fontWeight="bold" color="#555">
+                플레이 스타일
+              </FormLabel>
+              <CheckboxGroup>
+                <HStack spacing="24px">
+                  <Checkbox value="attacker">
+                    <Text>돌격형</Text>
+                  </Checkbox>
+                  <Checkbox value="defender">
+                    <Text>방어형</Text>
+                  </Checkbox>
+                  <Checkbox value="serious">
+                    <Text>빡겜</Text>
+                  </Checkbox>
+                  <Checkbox value="casual">
+                    <Text>즐겜</Text>
+                  </Checkbox>
+                </HStack>
+                <HStack spacing="24px" mt="2">
+                  <Checkbox value="voice">
+                    <Text>보이스</Text>
+                  </Checkbox>
+                  <Checkbox value="chat">
+                    <Text>채팅만</Text>
+                  </Checkbox>
+                  <Checkbox value="interact">
+                    <Text>소통</Text>
+                  </Checkbox>
+                  <Checkbox value="solo">
+                    <Text>솔플</Text>
+                  </Checkbox>
+                </HStack>
+              </CheckboxGroup>
+              <FormHelperText>
+                {currTab === "친구" ? "나" : "우리 파티"}의 플레이 스타일을
+                선택해주세요
+              </FormHelperText>
+            </>
+          )}
         </FormControl>
         <FormControl
           sx={{
@@ -133,10 +188,32 @@ const NewProfile = () => {
           }}
         >
           <FormLabel mt="8" fontWeight="bold" color="#555">
-            자기소개
+            {currTab === "친구" ? "자기" : currTab === "파티" ? "파티" : "길드"}{" "}
+            소개
           </FormLabel>
-          <Textarea placeholder="자기소개를 입력해주세요" />
-          <FormHelperText>나를 예비 친구들에게 소개해보세요</FormHelperText>
+          <Textarea
+            placeholder={
+              currTab === "친구"
+                ? "자기 소개를 입력해주세요"
+                : currTab === "파티"
+                ? "파티 소개를 입력해주세요"
+                : "길드 소개를 입력해주세요"
+            }
+          />
+          <FormHelperText>
+            {currTab === "친구"
+              ? "나"
+              : currTab === "파티"
+              ? "우리 파티"
+              : "우리 길드"}
+            를 예비{" "}
+            {currTab === "친구"
+              ? "친구"
+              : currTab === "파티"
+              ? "파티원"
+              : "길드원"}
+            들에게 소개해보세요
+          </FormHelperText>
         </FormControl>
         <Flex justify="end" mt="4">
           <Button size="md">만들기</Button>
